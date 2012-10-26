b0ec9991354e85995cc457d953baeead90d0f5e5
Aamir Mansoor <me@aam1r.com>
2012-10-26 06:04:16 -0400

    Create and associate Markdown model with Pluggable UI
    - Create a model for Markdown that will hold the appropriate data for
      Markdown files. It will also provide functionality to render Markdown
      as HTML
    - Associate the MarkdownReviewUI to the Markdown model
    - Make the 'markdown' package an installation requirement for
      Review Board

--- reviewboard/reviews/ui/markdown.py
+++ reviewboard/reviews/ui/markdown.py
@@ -1,8 +1,9 @@
 from reviewboard.reviews.ui.base import ReviewUI
+from reviewboard.reviews.models import Markdown
 
 
 class MarkdownReviewUI(ReviewUI):
-    model = None
+    model = Markdown
     comment_model = None
     template_name = 'reviews/ui/markdown.html'
     object_key = 'markdown'
