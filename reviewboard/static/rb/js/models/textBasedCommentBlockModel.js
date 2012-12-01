12888fcd3859897e8df455f9522bac0a21317607
Aamir Mansoor <me@aam1r.com>
2012-12-01 17:24:51 -0500

    Removed debug/unused code
    Removed code that was used to initially ensure that everything is
    working. Also removed code that wasn't being executed.
    
    Conflicts:
    	reviewboard/static/rb/js/views/markdownReviewableView.js

--- reviewboard/static/rb/js/models/textBasedCommentBlockModel.js
+++ reviewboard/static/rb/js/models/textBasedCommentBlockModel.js
@@ -1,20 +1,3 @@
 RB.TextCommentBlock = RB.FileAttachmentCommentBlock.extend({
-    defaults: _.defaults({
-        x: null,
-        y: null,
-        width: null,
-        height: null
-    }, RB.AbstractCommentBlock.prototype.defaults),
-
-    serializedFields: ['x', 'y', 'width', 'height'],
-
-    parse: function(fields) {
-        fields.x = parseInt(fields.x, 10) || undefined;
-        fields.y = parseInt(fields.y, 10) || undefined;
-        fields.width = parseInt(fields.width, 10) || undefined;
-        fields.height = parseInt(fields.height, 10) || undefined;
-
-        return fields;
-    }
+    defaults: _.defaults({}, RB.AbstractCommentBlock.prototype.defaults),
 });
-
--- reviewboard/static/rb/js/views/textBasedCommentBlockView.js
+++ reviewboard/static/rb/js/views/textBasedCommentBlockView.js
@@ -1,22 +1,9 @@
 RB.TextBasedCommentBlockView = RB.AbstractCommentBlockView.extend({
     className: 'selection',
 
-    initialize: function() {
-        console.log('initialize in textbasecommentblockview');
-    },
-
     renderContent: function() {
-        console.log('render content in textbasedcommentblockview');
-
         this._$flag = $('<div/>')
             .addClass('selection-flag')
             .appendTo(this.$el);
-    },
-
-    positionCommentDlg: function(commentDlg) {
-        commentDlg.positionToSide(this._$flag, {
-            side: 'b',
-            fitOnScreen: true
-        });
-    },
+    }
 });
