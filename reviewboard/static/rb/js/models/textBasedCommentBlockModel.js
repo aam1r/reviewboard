cc46426c10c905e77c10be467b11845db7c5ed66
Aamir Mansoor <me@aam1r.com>
2012-12-01 18:15:06 -0500

    Several changes to get comment creation to work
    - Introduce serialize_comments to fetch comments from the database,
      and then serialize them so they can be used in JS.
    - Fix inheritance for various text-block and Markdown classes
    - Rename 'attachmentID' to 'fileAttachmentID'. This way we are
      overriding the property that is set in the parent class rather than
      introducing a new property. This, essentially, fixes comment creation.
    
    Conflicts:
    	reviewboard/reviews/ui/markdownui.py
    	reviewboard/static/rb/js/models/markdownReviewableModel.js
    	reviewboard/static/rb/js/models/textBasedCommentBlockModel.js
    	reviewboard/templates/reviews/ui/markdown.html

--- reviewboard/static/rb/js/models/textBasedCommentBlockModel.js
+++ reviewboard/static/rb/js/models/textBasedCommentBlockModel.js
@@ -1,3 +1,18 @@
 RB.TextCommentBlock = RB.FileAttachmentCommentBlock.extend({
-    defaults: _.defaults({}, RB.AbstractCommentBlock.prototype.defaults),
+    defaults: _.defaults({
+        id: null
+    }, RB.FileAttachmentCommentBlock.prototype.defaults),
+
+    serializedFields: ['id'],
+
+    /*
+     * Parses the incoming attributes for the comment block.
+     *
+     * The fields are stored server-side as strings, so we need to convert
+     * them back to integers where appropriate.
+     */
+    parse: function(fields) {
+        fields.id = parseInt(fields.id, 10) || undefined;
+        return fields;
+    }
 });
