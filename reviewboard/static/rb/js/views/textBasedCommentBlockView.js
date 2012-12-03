9c08ec34d34939e4d64ceed0feab8f4fa031d0b5
Aamir Mansoor <me@aam1r.com>
2012-12-02 19:11:39 -0500

    Modified behaviour related to comment flag
    - Repositioned the dialog
    - A new comment dialog is not created if one already exists
    
    Conflicts:
    	reviewboard/static/rb/js/views/textBasedCommentBlockView.js
    	reviewboard/static/rb/js/views/textBasedReviewableView.js

---
+++ reviewboard/static/rb/js/views/textBasedCommentBlockView.js
@@ -0,0 +1,32 @@
+RB.TextBasedCommentBlockView = RB.AbstractCommentBlockView.extend({
+    className: 'selection',
+
+    renderContent: function() {
+        this._$ghostCommentFlag = $("<span/>")
+            .addClass("commentflag")
+            .append($("<span/>").addClass("commentflag-shadow"));
+
+        this._$innerFlag = $("<span/>")
+            .addClass("commentflag-inner")
+            .appendTo(this._$ghostCommentFlag);
+
+        this._$count = $("<span/>")
+            .appendTo(this._$innerFlag);
+
+        this.$el.append(this._$ghostCommentFlag);
+
+        this.model.on('change:count', this._updateCount, this);
+        this._updateCount();
+    },
+
+    positionCommentDlg: function(commentDlg) {
+        commentDlg.positionToSide(this._$ghostCommentFlag, {
+            side: 'r',
+            fitOnScreen: true
+        });
+    },
+
+    _updateCount: function() {
+        this._$count.text(this.model.get('count'));
+    }
+});
---
+++ reviewboard/static/rb/js/views/textBasedReviewableView.js
@@ -0,0 +1,73 @@
+/*
+ * Base support for displaying a review UI for text-based file attachments.
+ */
+RB.TextBasedReviewableView = RB.FileAttachmentReviewableView.extend({
+    commentBlockView: RB.TextBasedCommentBlockView,
+
+    events: {
+        'mouseenter .rendered-comment': '_onMouseEnter',
+        'mouseleave .rendered-comment': '_onMouseLeave',
+        'click .rendered-comment': '_onClick'
+    },
+
+    initialize: function() {
+        RB.FileAttachmentReviewableView.prototype.initialize.call(this);
+
+        this.on('commentBlockViewAdded', function(commentBlockView) {
+            this._addCommentBlock(commentBlockView);
+        }, this);
+    },
+
+    renderContent: function() {
+        this._$rendered = $(this.model.get('rendered'));
+        this._$wrappedComments = '';
+
+        this._applyCommentWrapper();
+        this.$el.html(this._$wrappedComments);
+
+        return this;
+    },
+
+    _applyCommentWrapper: function() {
+        var self = this;
+        var child_id = 0;
+
+        this._$rendered.each(function() {
+            var wrapper = $('<div />')
+                .attr('class', 'rendered-comment')
+                .attr('data-child-id', child_id++)
+                .append($(this));
+
+            self._$wrappedComments += wrapper[0].outerHTML;
+        });
+    },
+
+    _addCommentBlock: function(commentBlockView) {
+        var child_id = commentBlockView.model.get('child_id');
+        var child = this.$el.find("[data-child-id='" + child_id + "']");
+
+        child.prepend(commentBlockView.$el);
+    },
+
+    _onMouseEnter: function(evt) {
+        var wrapper = $(evt.target).closest('.rendered-comment');
+        wrapper.css('background-color', '#F0F0F0');
+    },
+
+    _onMouseLeave: function(evt) {
+        var wrapper = $(evt.target).closest('.rendered-comment');
+        wrapper.css('background-color', 'white');
+    },
+
+    _onClick: function(evt) {
+        var wrapper = $(evt.target).closest('.rendered-comment');
+
+        if (wrapper.has('.commentflag').length == 0) {
+            var child_id = wrapper.data('child-id');
+
+            this.createAndEditCommentBlock({
+                child_id: child_id
+            });
+        }
+    }
+});
