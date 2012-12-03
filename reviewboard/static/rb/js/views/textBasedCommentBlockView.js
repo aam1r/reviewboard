c43a08573374c5990bee3b99c22272f89713d12a
Aamir Mansoor <me@aam1r.com>
2012-12-02 21:00:06 -0500

    Fixed conflicts

--- reviewboard/static/rb/js/views/textBasedCommentBlockView.js
+++ reviewboard/static/rb/js/views/textBasedCommentBlockView.js
@@ -1,36 +1,6 @@
 RB.TextBasedCommentBlockView = RB.AbstractCommentBlockView.extend({
     className: 'selection',
 
-<<<<<<< HEAD
-    renderContent: function() {
-        this._$ghostCommentFlag = $("<span/>")
-            .addClass("commentflag")
-            .append($("<span/>").addClass("commentflag-shadow"));
-
-        this._$innerFlag = $("<span/>")
-            .addClass("commentflag-inner")
-            .appendTo(this._$ghostCommentFlag);
-
-        this._$count = $("<span/>")
-            .appendTo(this._$innerFlag);
-
-        this.$el.append(this._$ghostCommentFlag);
-
-        this.model.on('change:count', this._updateCount, this);
-        this._updateCount();
-    },
-
-    positionCommentDlg: function(commentDlg) {
-        commentDlg.positionToSide(this._$ghostCommentFlag, {
-            side: 'r',
-            fitOnScreen: true
-        });
-    },
-
-    _updateCount: function() {
-        this._$count.text(this.model.get('count'));
-    }
-=======
     initialize: function() {
         console.log('initialize in textbasecommentblockview');
     },
@@ -49,5 +19,4 @@ RB.TextBasedCommentBlockView = RB.AbstractCommentBlockView.extend({
             fitOnScreen: true
         });
     },
->>>>>>> d1142b9... Introduce "text-based comment block" for commenting
 });
