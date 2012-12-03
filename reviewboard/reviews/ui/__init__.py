7a793346d4b4c59063664af1d89aabfd29f13592
Aamir Mansoor <me@aam1r.com>
2012-12-02 22:13:51 -0500

    Removed remaining Markdown dependency

--- reviewboard/reviews/ui/__init__.py
+++ reviewboard/reviews/ui/__init__.py
@@ -5,11 +5,9 @@ def _register_review_uis(**kwargs):
     """Registers all bundled review UIs."""
     from reviewboard.reviews.ui.base import register_ui
     from reviewboard.reviews.ui.image import ImageReviewUI
-    from reviewboard.reviews.ui.markdownui import MarkdownReviewUI
 
 
     register_ui(ImageReviewUI)
-    register_ui(MarkdownReviewUI)
 
 
 initializing.connect(_register_review_uis)
